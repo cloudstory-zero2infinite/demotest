536c30fe58fd379a57b3fec7799ae273caeb5dd3
haripriya kusnur <haripriya.kusnur@cloudstory.ind.in>
2026-04-16 16:30:19 +0530

    Dropdown for asset data filtering

--- components/Icons.tsx
+++ components/Icons.tsx
@@ -133,3 +133,9 @@ export const ExclamationCircleIcon: React.FC<{className?: string}> = ({ classNam
         <path strokeLinecap="round" strokeLinejoin="round" d="M12 8v4m0 4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
     </svg>
 );
+
+export const FilterIcon: React.FC<{className?: string}> = ({ className }) => (
+    <svg xmlns="http://www.w3.org/2000/svg" className={className} fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
+        <path strokeLinecap="round" strokeLinejoin="round" d="M3 4a1 1 0 011-1h16a1 1 0 011 1v2.586a1 1 0 01-.293.707l-6.414 6.414a1 1 0 00-.293.707V17l-4 4v-6.586a1 1 0 00-.293-.707L3.293 7.293A1 1 0 013 6.586V4z" />
+    </svg>
+);
